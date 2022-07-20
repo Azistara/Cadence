ad69df88bdaaf1d7520cfb7de64d6d057f8cf045
CleveyP <clevelandplonsey@gmail.com>
2022-07-20 01:20:10 -0700

    implement pause button

--- mp3_player.js
+++ mp3_player.js
@@ -1,10 +1,22 @@
 //play mp3s
+//HTMLMediaElement
+var audio = new Audio('./resources/mp3s/test.mp3');
 
-let playMp3 = () => {
-    var audio = new Audio('./resources/mp3s/test.mp3');
+let playMp3 = (event) => {
+   //play the audio
     audio.play();
+  //change the audio button to a pause button
+  event.target.removeEventListener('click', playMp3);
+  event.target.addEventListener('click', pauseMp3);
+  event.target.innerText = 'Pause';
   };
-
+let pauseMp3 = (event) =>{
+  audio.pause();
+  //change the audio button to a play button
+  event.target.removeEventListener('click', pauseMp3);
+  event.target.addEventListener('click', playMp3);
+  event.target.innerText = 'Play';
+}
  let playButton =  document.getElementById("play-button");
  playButton.addEventListener('click', playMp3);
-
+ 
