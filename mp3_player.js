6fa9e2546c1ca7222922b3d85cc8e6dc714982a8
CleveyP <clevelandplonsey@gmail.com>
2022-07-23 02:30:34 -0700

    volume button displays slider

--- mp3_player.js
+++ mp3_player.js
@@ -1,9 +1,12 @@
 //play mp3s
+
 //HTMLMediaElement
 var audio = new Audio('./resources/mp3s/test.mp3');
 let playButton =  document.getElementById("play-button");
 let stopButton = document.getElementById('stop-button');
+let volumeButton = document.getElementById('volume-button');
 let volumeSlider = document.getElementById('volume-slider');
+volumeSlider.style.display = 'none';
 
 let searchButton = document.getElementsByClassName('search-button')[0]; 
 let searchBar = document.getElementById('search-bar');
@@ -23,21 +26,26 @@ let pauseMp3 = () =>{
   playButton.addEventListener('click', playMp3);
   playButton.innerText = 'Play';
 };
-
-
 playButton.addEventListener('click', playMp3);
+
 
 let stopMp3 = (event) =>{
   pauseMp3(); //pause the audio 
   audio.currentTime = 0; //reset to beginning of the song
 }
-
 stopButton.addEventListener('click', stopMp3);
 
-let toggleVolumeSlider = () =>{ //display  slider above the volume button
-  
-}
 
+let toggleVolumeSlider = () =>{ //display  slider above the volume button
+  if(volumeSlider.style.display === 'none'){
+    volumeSlider.style.display = 'block';
+    console.log("success!!");
+  }
+  else{
+    volumeSlider.style.display = 'none';
+  }
+}
+volumeButton.addEventListener('click', toggleVolumeSlider);
 
 let displaySearchBar = () =>{
   console.log("hello from search bar!");
