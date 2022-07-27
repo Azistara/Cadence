c70f5ba098766c90a59a7fa6d5cf84aff73d6326
CleveyP <clevelandplonsey@gmail.com>
2022-07-27 02:08:45 -0700

    implement loadMusicFolders in index.js

--- index.js
+++ index.js
@@ -5,6 +5,42 @@ const url = require('url');
 const path = require('path');
 const { setVibrancy } = require('electron-acrylic-window');
 const { app } = require('electron');
+const os = require ('os');
+let fs = require('fs');
+
+const username = os.userInfo().username;
+let musicFolderPaths = [];
+let filename = `C:/Users/${username}/AppData/Roaming/Microsoft/Windows/Libraries/Music.library-ms`;
+
+
+function loadMusicFolders() {  //TODO: should create new file called users_music_folders_paths.txt and populate it with music folder paths
+   
+    //Check if file exists
+    if(fs.existsSync(filename)) {
+       let data = fs.readFileSync(filename, 'utf8').split('\n') //splits the xml file into lines
+       
+       data.forEach((line) => {
+        console.log(line + '\n');
+         if(line.includes('<url>') && line.includes('</url>')){ //if the line is a path to a music folder   && line.substring(line.length-6, line.length === '</url>'
+            let currentFolder = line.trim().substring(5, line.length-15);
+            if(currentFolder.substring(0, 11 ) !== "knownfolder"){
+            musicFolderPaths.push(currentFolder); //add the folder path to the musicFoldersPaths
+            }
+         }
+       });
+       musicFolderPaths.forEach((el) =>{
+          console.log(el + '\n');
+       });
+    
+    } else {
+       console.log("File Doesn\'t Exist.");
+    }
+ }
+ 
+ 
+
+
+
 
 
 //create the wondow acrylic options object
@@ -20,15 +56,16 @@ op = {
 
 const createWindow = () => {
   const win = new BrowserWindow({
-    frame: false,
+    frame: true,
   
     width: 1000,
     height: 1000,
   })
   setVibrancy(win, op); 
   win.loadFile('index.html');
-  win.removeMenu();
+  //win.removeMenu();
 }
 app.whenReady().then(() => {
-  createWindow()
-})
+  createWindow();
+  loadMusicFolders(); //run this command only if the folder "user_music does not already exist in the CADENCE project folder..."
+});
--- load_music.js
+++ load_music.js
@@ -1,47 +1,30 @@
 const os = require ('os');
-
 let fs = require('fs');
 
-const username = os.userInfo ().username;
+const username = os.userInfo().username;
+let musicFolderPaths = [];
 let filename = `C:/Users/${username}/AppData/Roaming/Microsoft/Windows/Libraries/Music.library-ms`;
-let sno = 0;
 
-$('#add-to-list').on('click', () => {
-   let name = $('#Name').val()
-   let email = $('#Email').val()
 
-   fs.appendFile('contacts', name + ',' + email + '\n')
-
-   addEntry(name, email)
-})
-
-function addEntry(name, email) {
-   if(name && email) {
-      sno++
-      let updateString = '<tr><td>'+ sno + '</td><td>'+ name +'</td><td>' 
-         + email +'</td></tr>'
-      $('#contact-table').append(updateString)
-   }
-}
-
-function loadAndDisplayContacts() {  
+function loadMusicFolders() {  
    
     //Check if file exists
     if(fs.existsSync(filename)) {
-       let data = fs.readFileSync(filename, 'utf8').split('\n')
+       let data = fs.readFileSync(filename, 'utf8').split('\n') //splits the xml file into lines
        
-       data.forEach((contact, index) => {
-          let [ name, email ] = contact.split(',')
-          addEntry(name, email)
+       data.forEach((line) => {
+        console.log(line + '\n');
+         if((line.substring(0,5) === '<url>') && line.substring(line.length-6, line.length) === '</url>'){ //if the line is a path to a music folder
+            let currentFolder = line.substring(5, line.length-6);
+            if(currentFolder.substring(0, 11 ) !== "knownfolder"){
+            musicFolderPaths.push(currentFolder); //add the folder path to the musicFoldersPaths
+            }
+         }
        })
     
     } else {
-       console.log("File Doesn\'t Exist. Creating new file.")
-       fs.writeFile(filename, '', (err) => {
-          if(err)
-             console.log(err)
-       })
+       console.log("File Doesn\'t Exist.");
     }
  }
  
- loadAndDisplayContacts()
+ loadMusicFolders();
