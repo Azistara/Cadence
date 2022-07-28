6795b33e63efdd1a2d78200e4745a15949bf7c35
CleveyP <clevelandplonsey@gmail.com>
2022-07-28 01:15:48 -0700

    modify extract_song.js

--- extract_songs.js
+++ extract_songs.js
@@ -1,45 +1,68 @@
+const { TrackType } = require('music-metadata/lib/type');
 
 fs = require('fs');
 
 
-let  extractSongsFromFolder = async (dir) =>{  //gets all song files (m4a, mp3, wav ) and stores them in the all_songs folder in the CADENCE project folder
-//get the local all__songs folder
-let allSongsFolder;
+
 
 
 
 let isAudioFile = (file) =>{
+    let isAudio = false;
     switch(file.type){
-        case: ''
+        
+        case 'audio/mpeg' : //.mp3
+            isAudio = true;
+            break;
+        case 'audio/wav' : //.wav
+            isAudio = true;
+            break;
+        case 'audio/ogg' : //.ogg
+            isAudio =  true;
+            break;
+        case 'audio/aac' : //.aac
+            isAudio =  true;
+            break;
+        case 'audio/m4a' : //.m4a 
+            isAudio =  true;
+            break;
+        default:
+            isAudio =  false;
     }
+    return isAudio;
 }
 
 
 
 //loop through the passed in folder creating a song file for every song and push files to  all_songs folder
-    try {
-      const files = await fs.promises.readdir(dir);
-  
-      for (const file of files) {
-        const p = path.join(moveFrom, file);
-        const stat = await fs.promises.stat(p);
-  
-        if (stat.isFile()) {
-          console.log("'%s'  file.", fromPath);
-          //move this file into the allSongsFolder
-          allSongsFolder.push(file);
-        } else if (stat.isDirectory()) {
-          console.log("'%s' directory.", fromPath);
-          //move into the subdirectory and repeat the process
-          extractSongsFromFolder(file);
-        }
-      }
-    } catch (e) {
-      console.error(e);
-    }
 
+let extractSongs = (pathArray) =>{
+    let files;
+    if(pathArray == null){
+        console.log("No songs in music library...");
+        return;
+    }
+    pathArray.forEach(p => {
+        
+    });
+    if(fs.statSync(p).isDirectory()){ // if current path  is a directory
+        filenames = fs.readdirSync(directory_name); //add all files in the directory that are audio files to the files array. 
+        filenames.forEach(file, () =>{
+            if(isAudioFile(file)){
+                files.push(file);
+                console.log(file);
+            }
+        });
+    }
+    else{ //if the path is a file and is an audio file then push it into the files array.
+        if(isAudioFile(p)){ 
+            files.push(p);
+            console.log(file);
+        }
+    }
+    return files;
 }
 
 
 
-module.exports = {extractSongsFromFolder};
+module.exports = {extractSongsFromPaths};
