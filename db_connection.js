6c7a38409926b0ad4edf4b7a8719cdc019418d21
CleveyP <clevelandplonsey@gmail.com>
2022-08-08 20:13:16 -0700

    create db_connection module

---
+++ db_connection.js
@@ -0,0 +1,10 @@
+var mysql = require('mysql');
+const { mysqlSettings } = require('./mysql_settings');
+mysqlSettings.database='cadence';
+var connection = mysql.createConnection(mysqlSettings);
+
+connection.connect(function(err) {
+    if (err) throw err;
+});
+
+module.exports = connection;
