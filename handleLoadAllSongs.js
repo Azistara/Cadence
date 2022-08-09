ebda96bf28be1d9187fc09bf0c12ea09edd983ea
CleveyP <clevelandplonsey@gmail.com>
2022-08-08 20:14:20 -0700

    fix handleLoadAllSongs

--- handleLoadAllSongs.js
+++ handleLoadAllSongs.js
@@ -1,45 +1,32 @@
-const {mysqlSettings} = require('./mysql_settings');
-var mysql = require('mysql');
+const connection = require("./db_connection.js");
 
-
-const handleLoadAllSongs =  () => { 
-    var response;
-    //connect to the mysql server
-    mysqlSettings.database = 'cadence';
-    connection = mysql.createConnection(mysqlSettings);
-    connection.connect(function(err) {
-        if (err) {
-          console.error('error connecting: ' + err.stack);
-          return;
+const query = async () => {
+    let sql = 'SELECT * FROM allsongs';
+return new Promise((resolve, reject)=>{
+    connection.query(sql,  (error, results)=>{
+        if(error){
+            return reject(error);
         }
-        else{
-        //3. query the database and select all the song rows
-            let sql = 'SELECT * FROM allsongs';
-            connection.query(sql, (error, result, fields) => {  
-                if (error){
-                    console.log("error occurred querying db from main process"); 
-                    return;
-                }
-                else{
-                    response = result.map(v => Object.assign({}, v)); //convert each row in result to a js object and put them in an array called response
-                    console.log(response[1]);
-                    // close the connection to the database
-                    connection.end(function(err){ 
-                        if(err){
-                            console.log('error closing connection' + err.stack);
-                            return;
-                        }
-                        else{
-                            console.log("returning response:" + response[1]);
-                            console.log("returning response:");
-                            return response; 
-                        }
-                       
-                    });
-                }
-            }); 
-        }
+        return resolve(results);
     });
-}
+});
+
+};
+
+const handleLoadAllSongs = async () => { 
+          
+    // query the database and select all the song rows
+    let response = null;
+    try{
+        response = await query(); 
+        response = response.map(v => Object.assign({}, v)); //convert each row in result to a js object and put them in an array called response
+        return response;
+    }
+    catch (error){
+        console.log("error occurred querying db from main process" + error); 
+        return;
+    }
+}        
+
 
 module.exports = {handleLoadAllSongs};
