5ff869aa292d5a2d65cd757bdf979d4e0ee40ad8
CleveyP <clevelandplonsey@gmail.com>
2022-08-28 03:54:49 -0700

    expose searchAllSongs to renderer in preload.js

--- backend_js/preload.js
+++ backend_js/preload.js
@@ -1,5 +1,6 @@
 const { contextBridge, ipcRenderer } = require('electron');
 
 contextBridge.exposeInMainWorld('electronAPI', {
-    loadAllSongs: () =>  ipcRenderer.invoke('load-all-songs') 
+    loadAllSongs: () =>  ipcRenderer.invoke('load-all-songs'),
+    searchAllSongs: (searchTerm) =>  ipcRenderer.invoke('search-all-songs', searchTerm)
 });
