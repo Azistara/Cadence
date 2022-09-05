213c79ae94c3fd03019dd47169009b09cb81c2f8
CleveyP <clevelandplonsey@gmail.com>
2022-09-05 05:27:35 -0700

    expose loadArt to renderer through preload

--- backend_js/preload.js
+++ backend_js/preload.js
@@ -2,5 +2,6 @@ const { contextBridge, ipcRenderer } = require('electron');
 
 contextBridge.exposeInMainWorld('electronAPI', {
     loadAllSongs: () =>  ipcRenderer.invoke('load-all-songs'),
-    searchAllSongs: (searchTerm) =>  ipcRenderer.invoke('search-all-songs', searchTerm)
+    searchAllSongs: (searchTerm) =>  ipcRenderer.invoke('search-all-songs', searchTerm),
+    loadArt: (path) => ipcRenderer.invoke('load-art', path)
 });
